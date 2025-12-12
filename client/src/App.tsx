b88af6b562d42eadb0d14d27a9803db175ba76ab
MikeLowry-021 <abrahamsstanwin351@gmail.com>
2025-12-12 15:51:46 +0200

    Updated App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,4 @@
 import { Switch, Route, useLocation } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import { OnboardingModal } from "@/components/onboarding-modal";
@@ -64,27 +59,20 @@ function App() {
   };
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="light">
-        <TooltipProvider>
-          <div className="min-h-screen flex flex-col">
-            <Header />
-            <main className="flex-1">
-              <Router />
-            </main>
-            <Footer />
-          </div>
-          <Toaster />
-          {showOnboarding && (
-            <OnboardingModal
-              open={showOnboarding}
-              onComplete={handleOnboardingComplete}
-              userName={user?.firstName}
-            />
-          )}
-        </TooltipProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">
+        <Router />
+      </main>
+      <Footer />
+      {showOnboarding && (
+        <OnboardingModal
+          open={showOnboarding}
+          onComplete={handleOnboardingComplete}
+          userName={user?.firstName}
+        />
+      )}
+    </div>
   );
 }
 
