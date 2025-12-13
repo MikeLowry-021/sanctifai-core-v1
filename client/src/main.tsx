d3d2213d0f44541faec70f9264778b6b7ae50810
MikeLowry-021 <abrahamsstanwin351@gmail.com>
2025-12-13 15:46:04 +0200

    Updated main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/queryClient";
-import { ThemeProvider } from "@/components/theme-provider";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { Toaster } from "@/components/ui/toaster";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="light">
-      <TooltipProvider>
-        <App />
-        <Toaster />
-      </TooltipProvider>
-    </ThemeProvider>
-  </QueryClientProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
